dfcb88412b11b4d37b6415d7dfaf6ac98ac1872d
D3t3rm1nat1on <bro.chek@mail.ru>
2019-12-03 23:57:53 +0300

    CountSmileys

---
+++ Algorithms/MyAlgorithms.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Algorithms
+{
+    public static class MyAlgorithms
+    {
+        public static int CountSmileys(string[] smileys) 
+        {
+            return smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[-~]{0,1}[)D]{1}$"));
+        }
+    }
+}
