6095e284c2e2873f5ac1b3cf43e3479d2a7049b0
D3t3rm1nat1on <bro.chek@mail.ru>
2019-12-04 00:06:33 +0300

    some more algorithms

--- Algorithms/MyAlgorithms.cs
+++ Algorithms/MyAlgorithms.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace Algorithms
@@ -8,6 +9,37 @@ namespace Algorithms
         public static int CountSmileys(string[] smileys) 
         {
             return smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[-~]{0,1}[)D]{1}$"));
+        }
+        
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            var str = s.Where(c => s.Count(c1 => char.ToLower(c1) == char.ToLower(c)) == 1).ToList();
+            return str.Count > 0? str.First().ToString() : "";
+        }
+        
+        public static string GetReadableTime(int seconds)
+        {
+            return $"{seconds / 60 / 60:D2}:{seconds / 60 % 60:D2}:{seconds % 60:D2}";
+        }
+        
+        public static string High(string s)
+        {
+            return s.Split().OrderByDescending(s1 => s1.Select(Convert.ToInt32).Sum(i => i - 'a' + 1)).First();
+        }
+        
+        public static string ExpandedForm(long num) 
+        {
+            string res = null;
+            string temp = num.ToString();
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] != '0')
+                {
+                    res += $"{temp[i]}{new string('0',temp.Length - i - 1)} ";
+                }
+                
+            }
+            return string.Join(" + ", res.Trim().Split());
         }
     }
 }
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,7 +6,7 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(MyAlgorithms.ExpandedForm(123));
         }
     }
 }
